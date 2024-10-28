63c32f23d3c0069d2bae5c98a698d7c07afb8a06
Nafis Foysal Ashik <126339346+Nafis-Foysal-Ashik@users.noreply.github.com>
2024-10-28 22:39:09 +0600

    Add files via upload
    array.sh added

---
+++ array.sh
@@ -0,0 +1,21 @@
+arr=('nafis' 'foysal' 'ashik')
+
+echo "${arr[@]}" #print all the element of the array
+echo "${arr[0]}" #print the 0th index element
+echo "${!arr[@]}" #print all the index 
+echo "${#arr[@]}" #print the length of the array
+
+arr[3]='CSE-20' # add an element to the exixtsing array
+echo "${arr[@]}"
+
+arr[3]='KUET' #replace KUET-CSE to KUET
+
+echo "${arr[@]}"
+
+unset arr[3]
+echo "${arr[@]}"
+
+string=nafis # this value stored in the 0th index
+echo "${string[@]}"
+echo "${string[0]}"
+echo "${string[1]}"
